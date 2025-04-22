87e589c5a4256275f8d624c64a42850781e3cd5b
Brandon Harris <brandon.harris@bmw.de>
2025-04-22 16:26:22 +0200

    [7/11] Third round of review comments addressed

--- src/util/InputRangeUtils.h
+++ src/util/InputRangeUtils.h
@@ -26,7 +26,7 @@ namespace ad_utility {
 // 3. This class only yields an input range, independent of the range category
 // of the input.
 CPP_class_template(typename View, typename F)(requires(
-    ql::ranges::input_range<View>&&  // ql::ranges::view<View>&&
+    ql::ranges::input_range<View>&& ql::ranges::view<View>&&
         std::is_object_v<F>&&
             ranges::invocable<F, ql::ranges::range_reference_t<
                                      View>>)) struct CachingTransformInputRange
@@ -88,6 +88,9 @@ CPP_class_template(typename View, typename F)(requires(
 // This is the exact same way `std::ranges` and `range-v3` behave.
 template <typename Range, typename F>
 CachingTransformInputRange(Range&&,
+                           F) -> CachingTransformInputRange<all_t<Range>, F>;
+template <typename Range, typename F>
+CachingTransformInputRange(Range&&, ql::ranges::iterator_t<Range>,
                            F) -> CachingTransformInputRange<all_t<Range>, F>;
 
 namespace loopControl {
